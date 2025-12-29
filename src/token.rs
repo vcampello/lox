b6f2fa388cd5649bbe2d6888fa0eb4e8f1fb73ce
Victor Campello <victor.campello.work@gmail.com>
2025-12-29 12:06:48 +0000

    chore: improve TokenType Display

--- src/token.rs
+++ src/token.rs
@@ -80,57 +80,55 @@ impl TokenType {
 
 impl fmt::Display for TokenType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let value = match self {
+        match self {
             // Single-character tokens.
-            TokenType::LeftParen => "LeftParen",
-            TokenType::RightParen => "RightParen",
-            TokenType::LeftBrace => "LeftBrace",
-            TokenType::RightBrace => "RightBrace",
-            TokenType::Comma => "Comma",
-            TokenType::Dot => "Dot",
-            TokenType::Minus => "Minus",
-            TokenType::Plus => "Plus",
-            TokenType::Semicolon => "Semicolon",
-            TokenType::Slash => "Slash",
-            TokenType::Star => "Star",
+            TokenType::LeftParen => write!(f, "LeftParen"),
+            TokenType::RightParen => write!(f, "RightParen"),
+            TokenType::LeftBrace => write!(f, "LeftBrace"),
+            TokenType::RightBrace => write!(f, "RightBrace"),
+            TokenType::Comma => write!(f, "Comma"),
+            TokenType::Dot => write!(f, "Dot"),
+            TokenType::Minus => write!(f, "Minus"),
+            TokenType::Plus => write!(f, "Plus"),
+            TokenType::Semicolon => write!(f, "Semicolon"),
+            TokenType::Slash => write!(f, "Slash"),
+            TokenType::Star => write!(f, "Star"),
 
             // One or two character tokens.
-            TokenType::Bang => "Bang",
-            TokenType::BangEqual => "BangEqual",
-            TokenType::Equal => "Equal",
-            TokenType::EqualEqual => "EqualEqual",
-            TokenType::Greater => "Greater",
-            TokenType::GreaterEqual => "GreaterEqual",
-            TokenType::Less => "Less",
-            TokenType::LessEqual => "LessEqual",
+            TokenType::Bang => write!(f, "Bang"),
+            TokenType::BangEqual => write!(f, "BangEqual"),
+            TokenType::Equal => write!(f, "Equal"),
+            TokenType::EqualEqual => write!(f, "EqualEqual"),
+            TokenType::Greater => write!(f, "Greater"),
+            TokenType::GreaterEqual => write!(f, "GreaterEqual"),
+            TokenType::Less => write!(f, "Less"),
+            TokenType::LessEqual => write!(f, "LessEqual"),
 
             // Literals.
-            TokenType::Identifier => "Identifier",
-            TokenType::String(v) => &format!("String({})", &v),
-            TokenType::Number(v) => &format!("Number({})", &v),
+            TokenType::Identifier => write!(f, "Identifier"),
+            TokenType::String(v) => write!(f, "String({v})"),
+            TokenType::Number(v) => write!(f, "Number({v})"),
 
             // Keywords.
-            TokenType::And => "And",
-            TokenType::Class => "Class",
-            TokenType::Else => "Else",
-            TokenType::False => "False",
-            TokenType::Fun => "Fun",
-            TokenType::For => "For",
-            TokenType::If => "If",
-            TokenType::Nil => "Nil",
-            TokenType::Or => "Or",
-            TokenType::Print => "Print",
-            TokenType::Return => "Return",
-            TokenType::Super => "Super",
-            TokenType::This => "This",
-            TokenType::True => "True",
-            TokenType::Var => "Var",
-            TokenType::While => "While",
+            TokenType::And => write!(f, "And"),
+            TokenType::Class => write!(f, "Class"),
+            TokenType::Else => write!(f, "Else"),
+            TokenType::False => write!(f, "False"),
+            TokenType::Fun => write!(f, "Fun"),
+            TokenType::For => write!(f, "For"),
+            TokenType::If => write!(f, "If"),
+            TokenType::Nil => write!(f, "Nil"),
+            TokenType::Or => write!(f, "Or"),
+            TokenType::Print => write!(f, "Print"),
+            TokenType::Return => write!(f, "Return"),
+            TokenType::Super => write!(f, "Super"),
+            TokenType::This => write!(f, "This"),
+            TokenType::True => write!(f, "True"),
+            TokenType::Var => write!(f, "Var"),
+            TokenType::While => write!(f, "While"),
 
-            TokenType::Eof => "Eof",
-        };
-
-        write!(f, "{value}")
+            TokenType::Eof => write!(f, "Eof"),
+        }
     }
 }
 
