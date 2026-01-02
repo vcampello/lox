80d9b10396d7c21df9ea5985b40de3f5ef010d79
Victor Campello <victor.campello.work@gmail.com>
2026-01-02 22:58:47 +0000

    feat: implement equality expression

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,55 @@
-use crate::token::Token;
+use std::{iter::Peekable, slice::Iter};
+
+use crate::{
+    ast::expression::Expr,
+    token::{Token, TokenType},
+};
 
 pub struct Parser<'a> {
-    current: usize,
     tokens: &'a [Token],
+    iter: Peekable<Iter<'a, Token>>,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(tokens: &'a [Token]) -> Self {
-        Self { tokens, current: 0 }
+        Self {
+            tokens,
+            iter: tokens.iter().peekable(),
+        }
     }
-    fn advance(&mut self) {
-        //
+
+    fn expression(&mut self) -> Expr {
+        self.equality()
+    }
+
+    fn equality(&mut self) -> Expr {
+        let mut expr = self.comparison();
+
+        while let Some(token) = self.match_tokens(&[TokenType::Equal, TokenType::EqualEqual]) {
+            let operator = token.clone();
+            let right = self.comparison();
+            expr = Expr::new_binary(expr, operator, right)
+        }
+
+        expr
+    }
+
+    fn comparison(&self) -> Expr {
+        todo!()
+    }
+
+    fn check(&mut self, token_type: &TokenType) -> bool {
+        matches!(self.iter.peek(), Some(t) if t.token_type == *token_type)
+    }
+
+    fn match_tokens(&mut self, token_types: &[TokenType]) -> Option<&Token> {
+        for token_type in token_types.iter() {
+            if self.check(token_type) {
+                return self.iter.next();
+            }
+        }
+
+        None
     }
 }
 
--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum TokenType {
     // Single-character tokens.
     LeftParen,
