27110d2cc44c92e5e55874bce288baf7a73c4906
Victor Campello <victor.campello.work@gmail.com>
2026-01-03 21:18:57 +0000

    feat: implement more production rules

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use std::{iter::Peekable, slice::Iter};
 
 use crate::{
-    ast::expression::Expr,
+    ast::expression::{Expr, LiteralValue},
     token::{Token, TokenType},
 };
 
@@ -16,6 +16,20 @@ impl<'a> Parser<'a> {
             tokens,
             iter: tokens.iter().peekable(),
         }
+    }
+
+    fn check(&mut self, token_type: &TokenType) -> bool {
+        matches!(self.iter.peek(), Some(t) if t.token_type == *token_type)
+    }
+
+    fn match_tokens(&mut self, token_types: &[TokenType]) -> Option<&Token> {
+        for token_type in token_types.iter() {
+            if self.check(token_type) {
+                return self.iter.next();
+            }
+        }
+
+        None
     }
 
     fn expression(&mut self) -> Expr {
@@ -34,22 +48,100 @@ impl<'a> Parser<'a> {
         expr
     }
 
-    fn comparison(&self) -> Expr {
-        todo!()
-    }
+    fn comparison(&mut self) -> Expr {
+        let mut expr = self.term();
 
-    fn check(&mut self, token_type: &TokenType) -> bool {
-        matches!(self.iter.peek(), Some(t) if t.token_type == *token_type)
-    }
-
-    fn match_tokens(&mut self, token_types: &[TokenType]) -> Option<&Token> {
-        for token_type in token_types.iter() {
-            if self.check(token_type) {
-                return self.iter.next();
-            }
+        while let Some(token) = self.match_tokens(&[
+            TokenType::Greater,
+            TokenType::GreaterEqual,
+            TokenType::Less,
+            TokenType::LessEqual,
+        ]) {
+            let operator = token.clone();
+            let right = self.term();
+            expr = Expr::new_binary(expr, operator, right)
         }
 
-        None
+        expr
+    }
+
+    fn term(&mut self) -> Expr {
+        let mut expr = self.factor();
+
+        while let Some(token) = self.match_tokens(&[TokenType::Plus, TokenType::Minus]) {
+            let operator = token.clone();
+            let right = self.term();
+            expr = Expr::new_binary(expr, operator, right)
+        }
+
+        expr
+    }
+
+    fn factor(&mut self) -> Expr {
+        let mut expr = self.unary();
+
+        while let Some(token) = self.match_tokens(&[TokenType::Slash, TokenType::Star]) {
+            let operator = token.clone();
+            let right = self.term();
+            expr = Expr::new_binary(expr, operator, right)
+        }
+
+        expr
+    }
+
+    fn unary(&mut self) -> Expr {
+        match self.match_tokens(&[TokenType::Bang, TokenType::Minus]) {
+            Some(token) => {
+                let operator = token.clone();
+                let right = self.term();
+
+                Expr::new_unary(operator, right)
+            }
+            None => self.primary(),
+        }
+    }
+
+    fn primary(&mut self) -> Expr {
+        if self.match_tokens(&[TokenType::False]).is_some() {
+            return Expr::Literal(LiteralValue::Bool(false));
+        }
+
+        if self.match_tokens(&[TokenType::True]).is_some() {
+            return Expr::Literal(LiteralValue::Bool(true));
+        }
+
+        if self.match_tokens(&[TokenType::Nil]).is_some() {
+            return Expr::Literal(LiteralValue::Nil);
+        }
+
+        if self.match_tokens(&[TokenType::Nil]).is_some() {
+            return Expr::Literal(LiteralValue::Nil);
+        }
+
+        // REVIEW: this doesn't work as intended as the enum require values. I'll need to review the TokenType
+        // match self.match_tokens(&[TokenType::Number, TokenType::String]) {
+        //     Some(token) => {
+        //         let operator = token.clone();
+        //         let right = self.term();
+        //
+        //         return Expr::new_unary(operator, right);
+        //     }
+        //     None => (),
+        // };
+        //
+
+        if self.match_tokens(&[TokenType::LeftParen]).is_some() {
+            // TODO: use self.consume
+            // consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return Expr::new_grouping(self.expression());
+        }
+
+        // TODO: proper error handling
+        todo!("finish implementing Parser.primary");
+    }
+
+    pub fn parse(&mut self) -> Expr {
+        todo!();
     }
 }
 
